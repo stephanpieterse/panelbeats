b18b4bbcb21c16c0ba1df8ae482391b3254d46cd
Stephan Pieterse <stephan.pieterse@vcontractor.co.za>
2024-07-11 10:09:53 +0200

    Small repo cleanup
    Signed-off-by: Stephan Pieterse <stephan.pieterse@vcontractor.co.za>

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Panelbeats
 
-
 https://stephanpieterse.github.io/panelbeats/
+
+
+## Local running options
+`busybox httpd -p 8080 -f`
+`python3 -m http.server 8080`
--- page.js
+++ page.js
@@ -1,40 +1,6 @@
 let panels = 8;
 let sheet = "numbers";
 
-// let sheets = {
-//     "numbers": [
-//         "static/n1.jpg",
-//         "static/n2.jpg",
-//         "static/n3.jpg",
-//         "static/n4.jpg",
-//         "static/n5.jpg",
-//         "static/n6.jpg",
-//         "static/n7.jpg",
-//         "static/n8.jpg",
-//     ],
-//     "drums1": [
-//         "static/qnr.jpg",
-//         "static/qn.jpg",
-//         "static/8xx.jpg",
-//         "static/8-x.jpg",
-//     ],
-//     "drums2": [
-//         "static/qnr.jpg",
-//         "static/qn.jpg",
-//         "static/8xx.jpg",
-//         "static/8-x.jpg",
-//         "static/16xxxx.jpg",
-//         "static/16xxx-.jpg",
-//         "static/16xx-x.jpg",
-//         "static/16x-xx.jpg",
-//         "static/16-xxx.jpg",
-//         "static/16x--x.jpg",
-//         "static/16-x-x.jpg",
-//         "static/16-x--.jpg",
-//         "static/16---x.jpg",
-//     ]
-// };
-
 let initPage = function () {
     let sheetOptions = "";
     for (let s in sheets) {
